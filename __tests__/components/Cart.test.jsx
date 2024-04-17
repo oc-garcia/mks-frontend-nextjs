ff895dedf1c569785b82b8f7f33799acde4f752c
oc-garcia <octaviogarcia1988@gmail.com>
2024-04-17 18:45:32 -0300

    Refactor Jest configuration and add custom setup files

---
+++ __tests__/components/Cart.test.jsx
@@ -0,0 +1,19 @@
+// Cart.test.jsx
+import { render } from "@testing-library/react";
+import Cart from "../../src/components/default/Cart";
+
+describe("Cart Component", () => {
+  it("renders without crashing", () => {
+    const { container } = render(<Cart isOpen={false} onClose={() => {}} />);
+    expect(container).toBeTruthy();
+  });
+
+  it("displays 'Carrinho de Compras' when open", () => {
+    const { rerender, getByText } = render(<Cart isOpen={false} onClose={() => {}} />);
+
+    // Re-render the component with the drawer open
+    rerender(<Cart isOpen={true} onClose={() => {}} />);
+
+    expect(getByText("Carrinho de Compras")).toBeInTheDocument();
+  });
+});
--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,25 @@
-import type { Config } from "@jest/types";
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
-const config: Config.InitialOptions = {
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
   roots: ["<rootDir>"],
   moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
   testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
 
-export default config;
+export default createJestConfig(config);
