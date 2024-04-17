908bf431f0d2526b30711eade55503cd09bf480d
oc-garcia <octaviogarcia1988@gmail.com>
2024-04-17 18:27:26 -0300

    Add react-query dependency to package.json and import QueryClient and QueryClientProvider in providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 import { CartProvider } from "@/contexts/cart/CartProvider";
 import { theme } from "@/theme/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 
+// Create a client
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
