4cd17abe3bf2d3628533ae13cef80f26846b9884
oc-garcia <octaviogarcia1988@gmail.com>
2024-04-16 21:35:31 -0300

    Add react-icons dependency and update TopBar component in src/components/default/TopBar.tsx

--- src/components/default/TopBar.tsx
+++ src/components/default/TopBar.tsx
@@ -1,27 +1,44 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import styled from "@emotion/styled";
+import { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
 const StyledContainer = styled(Box)`
+  max-height: 101px;
+  display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 1rem;
   background-color: var(--chakra-colors-customBlue);
-  h1 {
+`;
+
+const Title = styled.h1`
+  color: var(--chakra-colors-white);
+  font-size: 40px;
+  font-weight: 600;
+  span {
+    font-size: 20px;
     color: var(--chakra-colors-white);
-    font-size: 40px;
-    font-weight: 600;
-    span {
-      color: var(--chakra-colors-white);
-      font-weight: 300;
-    }
+    font-weight: 300;
   }
 `;
 
+const StyledCartIcon = styled(FaShoppingCart)`
+  width: 19.01px;
+  height: 18px;
+  margin-right: 0.5rem;
+`;
+
 const TopBar = () => {
+  const [cartItems] = useState(0);
   return (
     <StyledContainer>
-      <h1>
+      <Title>
         MKS<span> Sistemas</span>
-      </h1>
+      </Title>
+      <Button leftIcon={<StyledCartIcon />} h={45} w={90} colorScheme="gray" color="black" variant="solid">
+        {cartItems}
+      </Button>
     </StyledContainer>
   );
 };
