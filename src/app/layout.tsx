09990b474bdfeaf8fccd8fb268f5e2aea6c3339a
Oc-Garcia <octaviogarcia1988@gmail.com>
2024-04-18 22:49:57 -0300

    Update function to handle edge cases

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import { Providers } from "./providers";
 import { fonts } from "./fonts";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className={fonts.montserrat.variable}>
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
